58407d9e226d48e3e0cf689282cc0982b930c603
asaprockky <shokirovabdulfayz@gmail.com>
2025-12-08 17:01:10 +0500

    user profile error has been fixed

--- routers/user_profile.py
+++ routers/user_profile.py
@@ -2,6 +2,7 @@ from datetime import datetime
 from http.client import HTTPException
 from fastapi import APIRouter, Depends
 from database.database import get_db
+from schemas.user_schema import UserProfileOut
 from sqlalchemy.orm import Session
 from database.models import User, UserProfile, StartedTest
 from routers.login import get_current_user
@@ -9,7 +10,7 @@ router = APIRouter()
 
 
 
-@router.get("users/me", response_model = UserProfile)
+@router.get("users/me", response_model = UserProfileOut)
 def user_profile(user : User = Depends(get_current_user), db: Session = Depends(get_db)):
     user_data = db.query(User).filter(User.id == user.id).first()
     if not user_data:
